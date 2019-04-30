0f1741426b066b35e2c00533ee37b6bb0b1ec69e
cdesilva <chris.a.desilva@gmail.com>
2019-04-30 11:46:58 -0500

    Style Game Setup components

--- src/components/SetTotalQuestions/SetTotalQuestions.js
+++ src/components/SetTotalQuestions/SetTotalQuestions.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 const SetTotalQuestions = props => (
     <div>
-        <form onSubmit={props.setTotal}>
-        <label>
-        Set Total # of Questions:
-        <input type="number" name="totalQuestions" min="1" max="25" onChange={props.changeTotal} />
-        </label>
-        <input 
-        type="submit" 
-        value="Set Total" 
-        />
-        </form>
+        <h2>Set Total Number of Questions</h2>
+        <Form onSubmit={props.setTotal}>
+            <Form.Group>
+                <Form.Control 
+                    type="number" 
+                    name="totalQuestions" 
+                    min="1" 
+                    max="25" 
+                    onChange={props.changeTotal} 
+                />
+            </Form.Group>
+        <Button type="submit">Set Total</Button>
+        </Form>
     </div>
 )
 
